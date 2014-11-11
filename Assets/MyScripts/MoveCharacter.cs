fd0e117d10a8e71fcaf657f84118887b6c6ab9e0
rogers14 <rogers14@miamioh.edu>
2014-11-11 15:10:36 -0500

    Initial Unity Checkin

---
+++ Assets/MyScripts/MoveCharacter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour 
+{
+	public float speed = 6.0F; 
+	public float jumpSpeed = 8.0F; 
+	public float gravity = 20.0F; 
+	private Vector2 moveDirection = Vector2.zero;
+	
+	void Start(){
+	}
+	
+	void Update(){
+		CharacterController player = GetComponent<CharacterController>();
+		
+		if (player.isGrounded)
+		{
+			// if player is on the ground, we normally don't want to move him vertically
+			moveDirection.y = 0;
+			
+			// but if the jump button is pressed, then we do want him moving vertically
+			if (Input.GetButton("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+			}
+		}
+		else
+		{
+			// if player is not on the ground, then apply gravity to him
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+		
+		// constantly move horizontally
+		moveDirection.x = speed;
+		
+		// finally, we actually apply the movement to the player
+		player.Move(moveDirection * Time.deltaTime);
+	}
+}
