223a4537f49d60391fb5f4431f5b67c445dde975
rogers14 <rogers14@miamioh.edu>
2014-11-13 14:04:33 -0500

    Finish platform
    Now when you land on the platform, GUIText says you win (only way to
    clear it currently to to quit the game) -- but it gets the point across.
    Also, I made it look prettier!

---
+++ Assets/MyScripts/FinishLine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	public GUIText finishText;
+
+	void Start()
+	{
+		finishText.text = "";
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		finishText.text = "You Win!";
+	}
+}
