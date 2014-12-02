5a5bb1b159c3c724185a3ddcf9bc671d77fb78e8
bartunnr <bartunnr@miamioh.edu>
2014-12-02 11:50:37 -0500

    Level Changes/Enemy Changes

---
+++ Assets/MyScripts/EnemyScriptUPDOWN.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScriptUPDOWN : MonoBehaviour {
+
+	CharacterDeath cd;
+	public float speed = .05f;
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		cd = GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterDeath> ();
+		startPos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = gameObject.transform.position;
+		pos.y -= speed;
+		gameObject.transform.position = pos;
+
+		if (pos.y < -9 || pos.y > 4.15)
+		{
+			speed = -speed;
+		}
+	}
+
+	void OnTriggerEnter (Collider c) 
+	{
+		if (c.tag == "Player")
+		{
+			cd.die();
+			//Destroy(gameObject);
+		}
+	}
+
+	// BUGS -- spawns a bunch of enemies instead of resetting the original
+	// Also, doesn't work if enemy was killed . . .
+	public void Reset()
+	{
+		gameObject.transform.position = startPos;
+	}
+}
